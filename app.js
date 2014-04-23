2091f6951dfac6d8871c7bf9bf07cd7ae45ac175
joachim verbeke <joachim.verbeke@wunderkraut.com>
2014-04-23 23:26:09 +0200

    hello world

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// coming soon. some awesome code for this file.
+/**
+ * The http module will help us to send and receive http requests
+ *
+ * @url http://nodejs.org/api/http.html
+ */
+var http = require("http");
+
+/**
+ * Creating our server through the createServer method
+ */
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-type":"text/plain"});
+  response.write("Hello world!");
+  response.end();
+});
+
+/**
+ * we need to declare a host and port and listen
+ */
+
+var host = "127.0.0.1"; // localhost
+var port = 2014;
+
+server.listen(port, host, function() {
+  console.log('listening');
+});
