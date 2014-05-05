8881d1c3555bab5e2becc9a80f1aca4fd2dfebdd
joachim verbeke <joachim.verbeke@wunderkraut.com>
2014-05-05 21:31:57 +0200

    express app

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-/**
- * The http module will help us to send and receive http requests
- *
- * @url http://nodejs.org/api/http.html
- */
-var http = require("http");
+// require modules
+var express = require('express');
+var http = require('http');
 
-/**
- * Creating our server through the createServer method
- */
-var server = http.createServer(function(request, response) {
-  if (request.url === '/hello-world') {
-    response.writeHead(200, {"Content-type":"text/plain"});
-    response.write("Hello world!");
-    response.end();
-  } else {
-    response.writeHead(404, {"Content-type":"text/plain"});
-    response.write("404 - Page not found");
-    response.end();
-  }
+// create our express app
+var app = express();
+
+// app configuration
+
+// setting the port
+app.set('port', process.env.PORT || 3000);
+
+
+// setting routes:
+// the method that we call is also the http method
+// first pass the route (url),
+// then determine what happens when visiting that url
+// callback function has request and response object
+// request is (off course) the request made to the server
+// response is what happens
+app.get("/hello-web", function (req, res) {
+  res.send("Hello Web");
 });
 
-/**
- * we need to declare a host and port and listen
- */
+// homepage
+app.get('/', function (req, res) {
+  res.send('homepage');
+});
 
-var host = "127.0.0.1"; // localhost
-var port = 2014;
+// adding html
+app.get("/about", function (req, res) {
+  var message = [
+    "<h1>About this site</h1>",
+    "<p>I'll be creating a blog in nodejs and then I'll use that blog to blog about the creation of the blog.</p>",
+    "<p>All very meta, I know ;-).",
+    "<p>I want to learn nodejs for the following reasons:</p>",
+    "<ul>",
+    "<li>I want to get a better understanding of the web</li>",
+    "<li>I want to get better at Javascript</li>",
+    "<li>learning something new is fun</li>",
+    "</ul>",
+    "<p>&nbsp;</p>",
+    "<p>Joachim</p>"].join('\n');
+  res.send(message);
+});
 
-server.listen(port, host, function() {
-  console.log('listening');
+// createServer creates a default http server
+// the app is passed to createServer, and sets it up
+// pass a callback function to the app after the server has begun
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express listening on port " + app.get('port'));
 });
